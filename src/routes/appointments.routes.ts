19019b42a5e9ebde885cafe384e6f6f527c3b004
Michael Moura <mikemorcerf@gmail.com>
2020-04-14 15:01:47 -0400

    add models and controllers

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+// Getting routes starting with '/appointments'
+appointmentsRouter.post('/', (request, response) => {
+	const { provider, date } = request.body;
+
+	const parsedDate = startOfHour(parseISO(date));
+
+	const findAppointmentInSameDate = appointmentsRepository.findByDate(
+		parsedDate,
+	);
+
+	if (findAppointmentInSameDate) {
+		return response
+			.status(400)
+			.json({ message: 'This appointment is already booked' });
+	}
+
+	const appointment = appointmentsRepository.create(provider, parsedDate);
+
+	return response.json(appointment);
+});
+
+export default appointmentsRouter;
