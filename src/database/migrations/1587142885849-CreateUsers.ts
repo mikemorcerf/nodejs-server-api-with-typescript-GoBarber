61f13f093abe30c1f30a83862e4c3b087acc32c2
Michael Moura <mikemorcerf@gmail.com>
2020-04-17 15:50:26 -0400

    establish relationship between appointments and users

--- src/database/migrations/1587142885849-CreateUsers.ts
+++ src/database/migrations/1587142885849-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1587142885849 implements MigrationInterface {
 				columns: [
 					{
 						name: 'id',
-						type: 'varchar',
+						type: 'uuid',
 						isPrimary: true,
 						generationStrategy: 'uuid',
 						default: 'uuid_generate_v4()',
