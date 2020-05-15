991aa94886cd3051e98bb352022323fe8b7b782e
Michael Moura <mikemorcerf@gmail.com>
2020-05-15 13:15:05 -0400

    implement Provider dashboard with appointment calendar

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -4,12 +4,14 @@ import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAut
 import ProvidersController from '../controllers/ProvidersController';
 import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const providersRouter = Router();
 
 const providersController = new ProvidersController();
 const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 providersRouter.use(ensureAuthenticated);
 
@@ -23,6 +25,11 @@ providersRouter.get(
 providersRouter.get(
 	'/:provider_id/month-availability',
 	providerMonthAvailabilityController.index,
+);
+
+providersRouter.get(
+	'/:provider_id/appointments',
+	providerAppointmentsController.index,
 );
 
 export default providersRouter;
