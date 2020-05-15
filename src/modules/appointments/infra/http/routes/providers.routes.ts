b847cb3589ba4b3ffb6f6689c93959adc8252d24
Michael Moura <mikemorcerf@gmail.com>
2020-05-15 19:01:55 -0400

    add request data validation to all routes using celebrate

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,17 +1,16 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
 import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
-import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const providersRouter = Router();
 
 const providersController = new ProvidersController();
 const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
-const providerAppointmentsController = new ProviderAppointmentsController();
 
 providersRouter.use(ensureAuthenticated);
 
@@ -19,17 +18,22 @@ providersRouter.get('/', providersController.index);
 
 providersRouter.get(
 	'/:provider_id/day-availability',
+	celebrate({
+		[Segments.PARAMS]: {
+			provider_id: Joi.string().uuid().required(),
+		},
+	}),
 	providerDayAvailabilityController.index,
 );
 
 providersRouter.get(
 	'/:provider_id/month-availability',
+	celebrate({
+		[Segments.PARAMS]: {
+			provider_id: Joi.string().uuid().required(),
+		},
+	}),
 	providerMonthAvailabilityController.index,
-);
-
-providersRouter.get(
-	'/:provider_id/appointments',
-	providerAppointmentsController.index,
 );
 
 export default providersRouter;
