6e7bd41a9cbfd257cecc012ad1f4e63cb5a03740
Michael Moura <mikemorcerf@gmail.com>
2020-05-15 15:21:36 -0400

    create Notification for service provider upon user has made an appointment with service provider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -12,6 +12,9 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 
+import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+
 container.registerSingleton<IUsersRepository>(
 	'UsersRepository',
 	UsersRepository,
@@ -25,4 +28,9 @@ container.registerSingleton<IAppointmentsRepository>(
 container.registerSingleton<IUserTokensRepository>(
 	'UserTokensRepository',
 	UserTokensRepository,
+);
+
+container.registerSingleton<INotificationsRepository>(
+	'NotificationsRepository',
+	NotificationsRepository,
 );
