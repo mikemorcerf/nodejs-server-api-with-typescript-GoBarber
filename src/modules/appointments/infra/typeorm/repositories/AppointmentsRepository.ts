90b79de0e57079cb3ae7a302879b2a0eae0668d5
Michael Moura <mikemorcerf@gmail.com>
2020-05-23 14:26:35 -0400

    show information about an user to providers

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -65,6 +65,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
 						`to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
 				),
 			},
+			relations: ['user'],
 		});
 
 		return appointmentsInDayFromProvider;
