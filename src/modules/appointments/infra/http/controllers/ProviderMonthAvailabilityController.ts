5c57ea1a96bda335bf8a849a5664bbcb4ac5c8ae
Michael Moura <mikemorcerf@gmail.com>
2020-05-23 12:19:11 -0400

    fix get params from body to query

--- src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -6,7 +6,7 @@ import ListProviderMonthAvailabilityService from '@modules/appointments/services
 export default class ProviderMonthAvailabilityController {
 	public async index(request: Request, response: Response): Promise<Response> {
 		const { provider_id } = request.params;
-		const { year, month } = request.body;
+		const { year, month } = request.query;
 
 		const listProviderMonthAvailability = container.resolve(
 			ListProviderMonthAvailabilityService,
@@ -15,8 +15,8 @@ export default class ProviderMonthAvailabilityController {
 		const providerMonthAvailability = await listProviderMonthAvailability.execute(
 			{
 				provider_id,
-				year,
-				month,
+				year: Number(year),
+				month: Number(month),
 			},
 		);
 
